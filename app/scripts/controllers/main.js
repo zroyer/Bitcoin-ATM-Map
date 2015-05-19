6a827b4bf9710997d2bc41b09015bd140fad2ebd
Zach Royer <zroyer@gmail.com>
2015-05-18 23:24:36 -0500

    trying to get marker labels working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,41 @@
  */
 angular.module('bitcoinAtmMapApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.map = {
+      center: {
+        latitude: 30.2669444, 
+        longitude: -97.7427778
+      },
+      zoom: 10
+    },
+
+    $scope.markerList = [
+      {
+        coords: {
+          latitude: 30.2669444,
+          longitude: -97.7427778
+        },
+        idKey:1,
+        message:"message 1",
+      },
+      {
+        coords: {
+          latitude: 30.2669444,
+          longitude: -97.7437778
+        },
+        options: { title: "title 2"},
+        idKey:2
+      },
+      {
+        coords: {
+          latitude: 30.2679444,
+          longitude: -97.7427778
+        },
+        options: { title: "title 3"},
+        idKey:3
+      }
+
+    ]
+
   });
