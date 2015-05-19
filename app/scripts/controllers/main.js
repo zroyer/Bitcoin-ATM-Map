8bf2593f19697732f005de4a39f8d2fe75e96f22
Zach Royer <zroyer@gmail.com>
2015-05-19 15:31:12 -0500

    making progress

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,11 +12,11 @@ angular.module('bitcoinAtmMapApp')
     
     $scope.map = {
       center: {
-        latitude: 30.4669444, 
+        latitude: 30.2679444, 
         longitude: -97.7427778
       },
       zoom: 10
-    },
+    };
 
     $scope.markerList = [
       {
@@ -24,26 +24,43 @@ angular.module('bitcoinAtmMapApp')
           latitude: 30.2669444,
           longitude: -97.8427778
         },
+        show: false,
+        visible: false,
         idKey:1,
-        message:"message 1",
+        title: 'Title 1',
+        address: 'address 1'
       },
       {
         coords: {
           latitude: 29.2669444,
           longitude: -97.7437778
         },
-        options: { title: "title 2"},
-        idKey:2
+        show: false,
+        visible: false,
+        idKey:2,
+        title: 'Title 2',
+        address: 'address 2'
       },
       {
         coords: {
           latitude: 30.2679444,
           longitude: -97.7427778
         },
-        options: { title: "title 3"},
-        idKey:3
+        show: false,
+        visible: false,
+        idKey:3,
+        title: 'Title 3',
+        address: 'address 3'
       }
+    ];
 
-    ]
+    $scope.onClick = function() {
+        $scope.visible = !$scope.visible;
+    };
+
+    $scope.closeClick = function() {
+        $scope.visible = false;
+    };
+
 
   });
